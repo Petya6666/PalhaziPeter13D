9a9487fe64ce63ddae7083b3f528bcda091e707c
palpet478 <palpet478@hengersor.hu>
2025-09-11 12:20:24 +0200

    appok csinalasa

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,31 @@ app.get("/", (req, res) => {
 res.send("Fut a backend!");
 })
 
+app.get("/regiok", (req, res) => {
+  const sql = "SELECT * FROM `regiok`";
+    db.query(sql, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result)})
+})
+app.get("/eszak", (req, res) => {
+    const sql = "SELECT * FROM `regiok` WHERE regionev LIKE 'Észak%'";
+      db.query(sql, (err, result) => {
+      if (err) return res.json(err);
+      return res.json(result)})
+  })
+app.get("/regioo", (req, res) => {
+    const sql = "SELECT * FROM `regiok` WHERE Rid >= 2";
+      db.query(sql, (err, result) => {
+      if (err) return res.json(err);
+      return res.json(result)})
+  })
+app.get("/tipus", (req, res) => {
+    const sql = "SELECT * FROM `regiok` WHERE regio_tipusa = 'régió'";
+      db.query(sql, (err, result) => {
+      if (err) return res.json(err);
+      return res.json(result)})
+  })
+
 app.listen(3001, () => {
 console.log("Server is running on port 3001");
 });
