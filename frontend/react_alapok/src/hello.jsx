b1800ce55545c29c4b32cad4f60b3102a40b7b46
palpet478 <palpet478@hengersor.hu>
2025-09-15 11:47:28 +0200

    torlesek

---
+++ frontend/react_alapok/src/hello.jsx
@@ -0,0 +1,16 @@
+
+function Hello() {
+
+    return (
+      <>
+  
+         <div>
+         <h1>Ez a hello oldal! :D</h1>
+         </div>
+  
+      </>
+    )
+  
+  }
+  
+  export default Hello
