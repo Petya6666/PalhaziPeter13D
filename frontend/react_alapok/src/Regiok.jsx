99fe6fe7c3854025b39a39f33ee6d5e90372e898
palpet478 <palpet478@hengersor.hu>
2025-09-18 13:53:59 +0200

    sok valtozas

--- frontend/react_alapok/src/Regiok.jsx
+++ frontend/react_alapok/src/Regiok.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Regiok() {
   return (
+    <>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="http://localhost:5173/Regisztracio">Regisztráció</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="http://localhost:5173/">Home</Nav.Link>
+            <Nav.Link href="http://localhost:5173/Regiok">Regiok</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     <div>
         <h1>Regiok</h1>
     </div>
+    </>
   )
 }
 export default Regiok
