7b7fcf6228af4444336b6c8feec48eb13498650b
palpet478 <palpet478@hengersor.hu>
2025-09-25 13:16:12 +0200

    kesz program kikommentelve

---
+++ panzio/server.js
@@ -0,0 +1,59 @@
+
+// Importáltam a szükséges csomagokat
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mysql = require('mysql');
+app.use(cors());
+
+// Adatbázis kapcsolat létrehozása
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    port: "3307",
+    database: "fogado"
+});
+
+// Alapértelmezett útvonal
+app.get("/", (req, res) => {
+    res.send("Fut a backend!");
+});
+
+// Szobák lekérdezése
+app.get("/fogado", (req, res) => {
+    const sql = "SELECT * FROM `szobak`";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})
+// Vendégek lekérdezése
+app.get("/kihasznaltsag", (req, res) => {
+    const sql = "SELECT szoba, COUNT(vendeg) AS vendegek, SUM(DATEDIFF(tav, erk)) AS vendegejszakak FROM foglalasok GROUP BY szoba ORDER BY vendegejszakak ASC, vendegek ASC;";
+    db.query(sql, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result)
+    })
+});
+// Foglaltság lekérdezése
+app.get("/foglaltsag", (req, res) => {
+    const sql = `
+        SELECT v.vnev AS név, f.erk AS érkezés, f.tav AS távozás 
+        FROM foglalasok f
+        JOIN vendegek v ON f.vendeg = v.vsorsz
+        WHERE f.szoba IN (SELECT szoba FROM szobak) 
+        ORDER BY v.vnev ASC;
+    `;
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+});
+
+
+
+// Szerver indítása a 3001-es porton
+app.listen(3001, () => {
+    console.log("Fut a szerver 3001-en");
+})
